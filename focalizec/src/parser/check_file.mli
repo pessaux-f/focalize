e1bc3d15dab3e003d6862e523a209c0ee57218de
weis <weis>
2007-07-11 10:23:21 +0000

    Introducing an interface for documentation purposes.

---
+++ focalizec/src/parser/check_file.mli
@@ -0,0 +1,5 @@
+(* $Id: check_file.mli,v 1.1 2007-07-11 08:23:21 weis Exp $ *)
+
+(** The focalize concrete syntax file checker.
+    This file defines the main verification procedure and runs it. *)
+
