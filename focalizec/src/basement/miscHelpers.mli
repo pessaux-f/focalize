c32e588ff432ca9da4065a9fd80bc428e7a81042
pessaux <pessaux>
2008-04-08 12:01:43 +0000

    Déplacement de miscHelper car il sert maintenant dès infer.ml.

---
+++ focalizec/src/basement/miscHelpers.mli
@@ -0,0 +1,20 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                        FoCaL compiler                               *)
+(*            François Pessaux                                         *)
+(*            Pierre Weis                                              *)
+(*            Damien Doligez                                           *)
+(*                               LIP6  --  INRIA Rocquencourt          *)
+(*                                                                     *)
+(*  Copyright 2007 LIP6 and INRIA                                      *)
+(*  Distributed only by permission.                                    *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: miscHelpers.mli,v 1.1 2008-04-08 10:01:43 pessaux Exp $ *)
+
+val bind_parameters_to_types_from_type_scheme :
+  Types.type_scheme option -> Parsetree.vname list ->
+    (((Parsetree.vname * Types.type_simple option) list) *
+      (Types.type_simple option) *
+      (Types.type_simple list))
