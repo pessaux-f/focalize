ba2769a37e868f4625cf7ccea03edb86de0343c6
pessaux <pessaux>
2008-09-16 17:10:20 +0000

    Suppression de débug.

--- focalizec/src/commoncodegen/abstractions.ml
+++ focalizec/src/commoncodegen/abstractions.ml
@@ -11,7 +11,7 @@
 (*                                                                     *)
 (***********************************************************************)
 
-(* $Id: abstractions.ml,v 1.38 2008-09-16 14:27:42 pessaux Exp $ *)
+(* $Id: abstractions.ml,v 1.39 2008-09-16 15:10:20 pessaux Exp $ *)
 
 
 (* ******************************************************************** *)
@@ -55,7 +55,7 @@ type environment_kind =
 
 
 
-(* For debugging purpose only. *)
+(* For debugging purpose only.
 let debug_print_dependencies_from_parameters l =
   List.iter
     (fun (species_param, methods) ->
@@ -71,7 +71,7 @@ let debug_print_dependencies_from_parameters l =
       Format.eprintf "@.")
     l
 ;;
-
+*)
 
 
 
@@ -1102,15 +1102,6 @@ let compute_abstractions_for_fields ~with_def_deps env ctx fields =
                    used_species_parameter_tys
                    dependencies_from_params_in_type
                    dependencies_from_params_via_compl in
-
-Format.eprintf "Pour théorème : %a@." Sourcify.pp_vname name ;
-Format.eprintf "dependencies_from_params_in_bodies:@." ;
-debug_print_dependencies_from_parameters dependencies_from_params_in_bodies ;
-Format.eprintf "dependencies_from_params_in_type:@." ;
-debug_print_dependencies_from_parameters dependencies_from_params_in_type ;
-Format.eprintf "dependencies_from_params_via_compl:@." ;
-debug_print_dependencies_from_parameters dependencies_from_params_via_compl ;
-
                let abstr_info = {
                  ai_used_species_parameter_tys =
                    all_used_species_parameter_tys ;
