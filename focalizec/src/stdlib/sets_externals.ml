ed93b974cf8579c92a177a19d601ea844ef00958
weis <weis>
2008-12-01 13:50:26 +0000

    Chasse au phoque. Banner.

--- focalizec/src/stdlib/sets_externals.ml
+++ focalizec/src/stdlib/sets_externals.ml
@@ -5,15 +5,16 @@
 (*            François Pessaux                                         *)
 (*            Pierre Weis                                              *)
 (*            Damien Doligez                                           *)
+(*                                                                     *)
 (*                               LIP6  --  INRIA Rocquencourt          *)
 (*                                                                     *)
-(*  Copyright 2007 LIP6 and INRIA                                      *)
+(*  Copyright 2007, 2008 LIP6 and INRIA                                *)
 (*  Distributed only by permission.                                    *)
 (*                                                                     *)
 (***********************************************************************)
 
-(* $Id: sets_externals.ml,v 1.2 2008-11-30 22:26:05 weis Exp $ *)
+(* $Id: sets_externals.ml,v 1.3 2008-12-01 12:50:26 weis Exp $ *)
 
 let i_failed r _elt =
-  raise (Ml_builtins.Foc_Failure ("Failed: " ^ r))
+  raise (Ml_builtins.Focalize_Failure ("Failed: " ^ r))
 ;;
--- focalizec/src/stdlib/sets_orders_externals.ml
+++ focalizec/src/stdlib/sets_orders_externals.ml
@@ -1,18 +1,20 @@
 (***********************************************************************)
 (*                                                                     *)
 (*                        FoCaLize compiler                            *)
+(*                                                                     *)
 (*            François Pessaux                                         *)
 (*            Pierre Weis                                              *)
 (*            Damien Doligez                                           *)
+(*                                                                     *)
 (*                               LIP6  --  INRIA Rocquencourt          *)
 (*                                                                     *)
-(*  Copyright 2007 LIP6 and INRIA                                      *)
+(*  Copyright 2007, 2008 LIP6 and INRIA                                *)
 (*  Distributed only by permission.                                    *)
 (*                                                                     *)
 (***********************************************************************)
 
-(* $Id: sets_orders_externals.ml,v 1.3 2008-09-24 08:52:21 weis Exp $ *)
+(* $Id: sets_orders_externals.ml,v 1.4 2008-12-01 12:50:26 weis Exp $ *)
 
 let i_failed r _elt =
-  raise (Ml_builtins.Foc_Failure ("Failed: " ^ r))
+  raise (Ml_builtins.Focalize_Failure ("Failed: " ^ r))
 ;;
