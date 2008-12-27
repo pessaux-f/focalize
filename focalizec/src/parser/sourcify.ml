8e2fd0e23409c69a0cb4f9308134ef5d2b0d84fd
weis <weis>
2008-12-27 23:05:49 +0000

    Sourcify must acknowledge the fact that Pf_auto [] is DOT.

--- focalizec/src/parser/sourcify.ml
+++ focalizec/src/parser/sourcify.ml
@@ -13,7 +13,7 @@
 (*                                                                     *)
 (***********************************************************************)
 
-(* $Id: sourcify.ml,v 1.63 2008-12-27 15:56:58 weis Exp $ *)
+(* $Id: sourcify.ml,v 1.64 2008-12-27 22:05:49 weis Exp $ *)
 
 open Parsetree;;
 
@@ -799,7 +799,7 @@ and pp_proof_desc ppf = function
       if enf_deps <> [] then Format.fprintf ppf " " ;
       Format.fprintf ppf "assumed@ {*%s*}@]" reason
   | Parsetree.Pf_auto [] ->
-      Format.fprintf ppf "@[<2>qed@]"
+      Format.fprintf ppf "@[<2>.@]"
   | Parsetree.Pf_auto facts ->
       Format.fprintf ppf "@[<2>by %a@]" (pp_facts "") facts
   | Parsetree.Pf_coq (enf_deps, s) ->
