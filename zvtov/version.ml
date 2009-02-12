29c15037ee6cb02e988366d6e6bc900b9e0416fb
doligez <doligez>
2009-02-12 11:42:06 +0000

    add a comment at the head of .pfc files

--- zvtov/version.ml
+++ zvtov/version.ml
@@ -1,4 +1,4 @@
 (*  Copyright 2005 INRIA  *)
-(*  $Id: version.ml,v 1.14 2008-11-25 15:59:31 doligez Exp $  *)
+(*  $Id: version.ml,v 1.15 2009-02-12 10:42:06 doligez Exp $  *)
 
-let version = "zvtov version 0.5.0 [29] 2008-11-25";;
+let version = "zvtov version 0.5.0 [30] 2009-02-05";;
