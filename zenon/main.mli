bb9aa5165a19ba58b5aa4f4f4c393f003d544c33
doligez <damien.doligez@inria.fr>
2013-02-22 14:36:37 +0100

    extract zenon into an independent repository; add it back as a submodule of focalize

--- zenon/main.mli
+++
@@ -1,4 +0,0 @@
-(*  Copyright 2000 INRIA  *)
-(*  $Id: main.mli,v 1.2 2004-04-29 13:04:52 doligez Exp $  *)
-
-(* This file left blank intentionally. *)
--- zenon/prove.mli
+++
@@ -1,7 +0,0 @@
-(*  Copyright 1997 INRIA  *)
-(*  $Id: prove.mli,v 1.3 2009-08-05 14:47:43 doligez Exp $  *)
-
-exception NoProof;;
-exception LimitsExceeded;;
-
-val prove : Expr.definition list -> (Expr.expr * int) list -> Mlproof.proof;;
