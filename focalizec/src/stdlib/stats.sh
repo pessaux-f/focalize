412d05be0ae90616245fb99a214aac11493c3575
Raphaël Cauderlier <cauderlier@crans.org>
2015-02-02 18:46:01 +0100

    Add a script to test coverage of the stdlib

---
+++ focalizec/src/stdlib/stats.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+function percent {
+    if [ $2 = '0' ]
+    then
+        echo 'NaN'
+    else
+        val=$(echo "scale = 2;  100 * $1 / $2" | bc -l)
+        echo "$val%"
+    fi
+}
+
+all_lines=$(cat *.fcl | wc -l)
+ok=
+
+# Test if the dk files is accepted by Dedukti
+for f in *.fcl
+do
+    d=${f%.*}.dk
+
+    echo
+    echo ${f%.*}
+    if dkcheck "$d"
+    then
+        ok="$ok $f"
+        echo 'OK'
+    else
+        echo 'KO'
+    fi
+    size=$(cat $f | wc -l)
+    echo "size: $size"
+    echo -n "Ignored: "
+    percent $(grep -c 'ASSUMED' $f) $size
+
+done
+
+ok_lines=$(cat $ok | grep -v 'ASSUMED' | wc -l)
+
+echo
+echo
+
+echo -n "OK: "
+percent $ok_lines $all_lines
+
+echo -n "ASSUMED: "
+percent $(cat *.fcl | grep -c 'ASSUMED') $all_lines
+
+echo -n "ASSUMED in OK: "
+percent $(cat $ok | grep -c 'ASSUMED') $all_lines
